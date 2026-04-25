bd5d1a94d0c20619692bd04511070848adefe7d1
Mukilan Ramakrishnan <mukilanramays2007@gmail.com>
2026-04-25 11:53:22 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,40 +1,20 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
-from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
-
+from cocotb.triggers import Timer
 
 @cocotb.test()
-async def test_project(dut):
+async def test_adder(dut):
     dut._log.info("Start")
-
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())
-
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
-
-    dut._log.info("Test project behavior")
-
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    
+    # Set input to 3 (binary 011 -> a=1, b=1, cin=0)
+    dut.ui_in.value = 3
+    await Timer(10, units="ns")
+    
+    # 1 + 1 + 0 = 2. So uo_out should be 2.
+    assert dut.uo_out.value == 2
+    
+    # Set input to 7 (binary 111 -> a=1, b=1, cin=1)
+    dut.ui_in.value = 7
+    await Timer(10, units="ns")
+    
+    # 1 + 1 + 1 = 3. So uo_out should be 3.
+    assert dut.uo_out.value == 3
