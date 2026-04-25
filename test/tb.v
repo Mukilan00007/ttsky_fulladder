cd913efd8c529ca7063133b18fae94c5820bbe99
Mukilan Ramakrishnan <mukilanramays2007@gmail.com>
2026-04-25 11:52:37 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,16 +1,18 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
+/* This testbench instantiates the module and makes wires 
+   available for cocotb.
+*/
 module tb ();
 
-  // Dump the signals to a FST file for GTKWave
+  // Dump signals for GTKWave
   initial begin
     $dumpfile("tb.fst");
     $dumpvars(0, tb);
     #1;
   end
 
-  // Signals for the DUT (Device Under Test)
   reg clk;
   reg rst_n;
   reg ena;
@@ -20,17 +22,8 @@ module tb ();
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
-
-  // Instantiate the user module
+  // Instantiate the project
   tt_um_example user_project (
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
       .ui_in   (ui_in),
       .uo_out  (uo_out),
       .uio_in  (uio_in),
